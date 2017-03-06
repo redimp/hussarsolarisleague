4b4953cdf76347d1279da2daa59a438f7b3ca0ca
Ralph Thesen <mail@redimp.de>
2017-03-06 11:13:30 +0100

    move port debug to config. replaced default config.py with a skeleton

--- run.py
+++ run.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 from hsl import app
-app.run(debug=True)
+app.run(debug=app.config['DEBUG'], port=app.config['PORT'])
--- run_profiler.py
+++ run_profiler.py
@@ -4,4 +4,4 @@ from werkzeug.contrib.profiler import ProfilerMiddleware
 
 app.config['PROFILE'] = True
 app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
-app.run(debug = True)
+app.run(debug=app.config['DEBUG'], port=app.config['PORT'])
