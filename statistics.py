ae2df2fb282000e20648e4bdadcce7ed68a23a9e
Ralph Thesen <ralph.thesen@scai.fhg.de>
2017-03-07 11:00:19 +0100

    count mechs and trials

--- statistics.py
+++ statistics.py
@@ -13,7 +13,13 @@ users_with_hangar = Hangar.query\
 games_running = Game.query.filter_by(status=2).count()
 games_finished = Game.query.filter_by(status=3).count()
 
+mechs_in_hangar = Hangar.query.count()
+trials_in_hangar = Hangar.query.filter_by(trial=True).count()
+
+
 print "Users total:", users_total
 print "Users with a hangar:", users_with_hangar
 print "Games running:", games_running
 print "Games finished:", games_finished
+print "Mechs in Hangar:", mechs_in_hangar
+print "Trials in Hangar:", trials_in_hangar
