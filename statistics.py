dcd233b5435d62dfd9a43e7285f155e94d129419
Christian Kuske <kuske@ins.uni-bonn.de>
2017-06-06 19:07:53 +0200

    adds win/loss ratio per class

--- statistics.py
+++ statistics.py
@@ -34,29 +34,6 @@ totalTonnage = sum([mech.chassis.weight for mech in Hangar.query.all()])
 print "Tonnage used (average): %d (%.2f)" % (usedTonnage, round(usedTonnage/2.0/games_finished,2))
 print "Tonnage total (average): %d (%.2f)" % (totalTonnage, round(totalTonnage/mechs_in_hangar,2))
 
-# win/loss ratio per tonnage
-games = Game.query.filter_by(status=3).all()
-wlPerTonnage = dict.fromkeys(range(20,101,5), 0.0+0.0j)
-for game in games:
-    # home is winner
-    if game.winner == game.player_home_id:
-        wlPerTonnage[game.mech_home.chassis.weight] += complex(1.0, 0.0)
-        wlPerTonnage[game.mech_away.chassis.weight] += complex(0.0, 1.0)
-    # away is winner
-    else:
-        wlPerTonnage[game.mech_away.chassis.weight] += complex(1.0, 0.0)
-        wlPerTonnage[game.mech_home.chassis.weight] += complex(0.0, 1.0)
-
-scorePerTonnage = dict.fromkeys(range(20,101,5), (0.0, 0.0, 0.0, 0.0))
-for tonnage, value in wlPerTonnage.iteritems():
-    if abs(value) > 0.0:
-        scorePerTonnage[tonnage] = (value.real/(value.real+value.imag), value.real, value.imag, value.real+value.imag)
-scorePerTonnage = sorted(scorePerTonnage.items(), key=operator.itemgetter(1), reverse=True)
-
-print "\nTonnage\t\tWin/Loss\tWin\tLoss\tUsage"
-for tonnage, value in scorePerTonnage:
-    print "%3d\t\t%6.2f\t\t%2d\t%3d\t%3d" % (tonnage, value[0], value[1], value[2], value[3])
-
 # total weights per user
 weightsAll = {}
 for user in User.query.all():
@@ -78,9 +55,31 @@ print "\n%-20s\t%s\t%s\t\t%s" % ("Player", "Tonnage (own)", "Tonnage (enemy)", "
 for user, value in weightsAll:
     print "%-20s\t%8d\t%8d\t\t%+6d" % (user, value[0], value[1], value[1]-value[0])
 
+# win/loss ratio per tonnage
+games = Game.query.filter_by(status=3).all()
+wlPerTonnage = dict.fromkeys(range(20,101,5), 0.0+0.0j)
+for game in games:
+    # home is winner
+    if game.winner == game.player_home_id:
+        wlPerTonnage[game.mech_home.chassis.weight] += complex(1.0, 0.0)
+        wlPerTonnage[game.mech_away.chassis.weight] += complex(0.0, 1.0)
+    # away is winner
+    else:
+        wlPerTonnage[game.mech_away.chassis.weight] += complex(1.0, 0.0)
+        wlPerTonnage[game.mech_home.chassis.weight] += complex(0.0, 1.0)
+
+scorePerTonnage = dict.fromkeys(range(20,101,5), (0.0, 0.0, 0.0, 0.0))
+for tonnage, value in wlPerTonnage.iteritems():
+    if abs(value) > 0.0:
+        scorePerTonnage[tonnage] = (value.real/(value.real+value.imag), value.real, value.imag, value.real+value.imag)
+scorePerTonnage = sorted(scorePerTonnage.items(), key=operator.itemgetter(1), reverse=True)
+
+print "\nTonnage\t\tWin/Loss\tWin\tLoss\tUsage"
+for tonnage, value in scorePerTonnage:
+    print "%4d\t\t%6.2f\t\t%2d\t%3d\t%3d" % (tonnage, value[0], value[1], value[2], value[3])
+
 # win/loss per chassis
 games = Game.query.filter_by(status=3).all()
-
 wlPerChassis = {}
 scorePerChassis = {}
 for mech in Chassis.query.all():
@@ -106,7 +105,36 @@ print "\nMech\t\t\tWin/Loss\tWin\tLoss\tUsage"
 for mech, value in scorePerChassis:
     print "%-15s\t\t%6.2f\t\t%2d\t%3d\t%3d" % (mech, value[0], value[1], value[2], value[3])
 
+# win/loss per class
+games = Game.query.filter_by(status=3).all()
+wlPerClass = {
+    'Light' : complex(0.0, 0.0),
+    'Medium' : complex(0.0, 0.0),
+    'Heavy' : complex(0.0, 0.0),
+    'Assault' : complex(0.0, 0.0)
+}
+scorePerClass = {
+    'Light' : (0.0, 0.0, 0.0, 0.0),
+    'Medium' : (0.0, 0.0, 0.0, 0.0),
+    'Heavy' : (0.0, 0.0, 0.0, 0.0),
+    'Assault' : (0.0, 0.0, 0.0, 0.0)
+}
 
+for game in games:
+    # home is winner
+    if game.winner == game.player_home_id:
+        wlPerClass[game.mech_home.chassis.weightclass] += complex(1.0, 0.0)
+        wlPerClass[game.mech_away.chassis.weightclass] += complex(0.0, 1.0)
+    # away is winner
+    else:
+        wlPerClass[game.mech_away.chassis.weightclass] += complex(1.0, 0.0)
+        wlPerClass[game.mech_home.chassis.weightclass] += complex(0.0, 1.0)
 
-        
-        
+for weightclass, value in wlPerClass.iteritems():
+    if abs(value) > 0.0:
+        scorePerClass[weightclass] = (value.real/(value.real+value.imag), value.real, value.imag, value.real+value.imag)
+scorePerClass = sorted(scorePerClass.items(), key=operator.itemgetter(1), reverse=True)
+
+print "\nClass\t\t\tWin/Loss\tWin\tLoss\tUsage"
+for weightclass, value in scorePerClass:
+    print "%-15s\t\t%6.2f\t\t%2d\t%3d\t%3d" % (weightclass, value[0], value[1], value[2], value[3])
